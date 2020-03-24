47dfb5cb06730e7570f9a2aec9c92a3982ae6dbe
S A Sohan <50193747+shn999@users.noreply.github.com>
2020-03-24 11:53:43 +0600

    Update memes.py

--- userbot/modules/memes.py
+++ userbot/modules/memes.py
@@ -628,7 +628,7 @@ async def univsaye(cowmsg):
 async def kek(keks):
     """ Check yourself ;)"""
     uio = ["/", "\\"]
-    for i in range(1, 95):
+    for i in range(1, 295):
         time.sleep(0.3)
         await keks.edit(":" + uio[i % 2])
 
@@ -707,7 +707,7 @@ async def slap(replied_user, event):
 async def lol(lel):
     """ Ok... """
     okay = "-_-"
-    for i in range(30):
+    for i in range(60):
         okay = okay[:-1] + "_-"
         await lel.edit(okay)
 
